36ff827ed3fcab31d5ef8411a9a02d957277651e
YASH PATIL <yashmahendrapatil@yashs-MacBook-Air.local>
2026-05-23 20:40:28 +0530

    fix: api url from env

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL || ''}/api`
+});
 
 export const projectsAPI = {
   getAll: () => api.get('/projects'),
